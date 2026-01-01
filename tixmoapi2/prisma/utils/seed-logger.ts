8b3841f352a5a57966e22b881c687872d5146e1b
Daniel Chapman <daniel.chapman@gmail.com>
2026-01-01 17:14:56 -0330

    fix: remove chalk from seed logger for cjs compatibility

--- tixmoapi2/prisma/utils/seed-logger.ts
+++ tixmoapi2/prisma/utils/seed-logger.ts
@@ -1,17 +1,16 @@
 /* eslint-disable no-console */
-import chalk from 'chalk';
 
 export const seedLogger = {
   info: (message: string, ...args: unknown[]) => {
-    console.log(chalk.blue('ℹ'), message, ...args);
+    console.log('ℹ', message, ...args);
   },
   success: (message: string, ...args: unknown[]) => {
-    console.log(chalk.green('✓'), message, ...args);
+    console.log('✓', message, ...args);
   },
   error: (message: string, ...args: unknown[]) => {
-    console.log(chalk.red('✗'), message, ...args);
+    console.log('✗', message, ...args);
   },
   warn: (message: string, ...args: unknown[]) => {
-    console.log(chalk.yellow('⚠'), message, ...args);
+    console.log('⚠', message, ...args);
   },
 };
