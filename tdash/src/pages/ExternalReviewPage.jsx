bfcc366085a93cf9a08060e92b2db72d07136bfb
Daniel Chapman <daniel.chapman@gmail.com>
2026-04-03 21:58:34 -0230

    Dim skeleton loading effect

--- tdash/src/pages/ExternalReviewPage.jsx
+++ tdash/src/pages/ExternalReviewPage.jsx
@@ -170,7 +170,7 @@ const DecisionActionButton = ({ option, saving, onClick }) => (
 );
 
 const ExternalReviewLoadingSkeleton = () => (
-    <SkeletonTheme baseColor="#26293a" highlightColor="#353a50">
+    <SkeletonTheme baseColor="#26293a" highlightColor="#1d2130">
         <div className="min-h-screen bg-[#141625] text-white">
             <div className="mx-auto max-w-[1500px] space-y-5 px-4 py-6 sm:px-6 lg:px-8">
                 <section className={`${panelClass} overflow-hidden px-6 py-8 sm:px-8`}>
